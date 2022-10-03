2c04bcfb886fa26dc33fe9efdce3d2e11242f4da
VCCFUND <you@example.com>
2022-10-03 10:40:46 +0200

    first commit

--- config/plugins.js
+++ config/plugins.js
@@ -2,7 +2,25 @@ module.exports = ({ env }) => ({
   "users-permissions": {
     config: {
       jwt: {
-        expiresIn: "1m",
+        expiresIn: "25m",
+      },
+    },
+  },
+
+  email: {
+    config: {
+      provider: "sendgrid",
+
+      providerOptions: {
+        apiKey: env("SENDGRID_API_KEY"),
+      },
+
+      settings: {
+        defaultFrom: "pawel.sluzbowy@interia.pl",
+
+        defaultReplyTo: "pawel.sluzbowy@interia.pl",
+
+        testAddress: "pawel.sluzbowy@interia.pl",
       },
     },
   },
